a767b8e7ce59d5551a74dc08113194692de7650f
durcakd <durcak.david01@gmail.com>
2014-04-22 17:21:31 +0200

    [main] update to use CoreController.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <QApplication>
 #include <CoreWindow.h>
 
+#include "CoreController.h"
+
 
 int main( int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-	CoreWindow coreWindow;
-	//coreWindow.resize( coreWindow.sizeHint());
-	coreWindow.resize( 400, 300 );
 
-	coreWindow.show();
+	CoreController coreController;
 
 	return app.exec();
 }
